cb5578b1a34ae9e4e5c0c046fd1df6f6b8bc2b2b
Peter Zhen <peterzhen0713@gmail.com>
2017-06-29 23:12:16 -0400

    remove onclick handler for expand

--- popup.js
+++ popup.js
@@ -2,6 +2,6 @@ const expandButton = document.getElementById("expand-popup");
 
 expandButton.onclick = () => {
   document.getElementById("hiddenDiv").innerHTML = "";
-  document.body.style.width = "100px";
-  document.getElementsByTagName("html")[0].style.width = "100px";
+  // document.body.style.width = "100px";
+  // document.getElementsByTagName("html")[0].style.width = "100px";
 };
