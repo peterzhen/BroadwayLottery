739dad6e07ff7c0d2ca6ca0f9cd5bf155da12d98
Peter Zhen <peterzhen0713@gmail.com>
2017-07-04 16:55:01 -0400

    allow storage between scripts

--- background.js
+++ background.js
@@ -1,28 +1,29 @@
-const fillForm = profile => {
-  const firstName = document.getElementById("dlslot_name_first");
-  const lastName = document.getElementById("dlslot_name_last");
-  const ticketQty = document.getElementById("dlslot_ticket_qty");
-  const email = document.getElementById("dlslot_email");
-  const month = document.getElementById("dlslot_dob_month");
-  const day = document.getElementById("dlslot_dob_day");
-  const year = document.getElementById("dlslot_dob_year");
-  const zip = document.getElementById("dlslot_zip");
-  const country = document.getElementById("dlslot_country");
-  const tos = document.getElementById("dlslot_agree");
+const fillForm = storage => {
+  if (storage.profile){
+    const firstName = document.getElementById("dlslot_name_first");
+    const lastName = document.getElementById("dlslot_name_last");
+    const ticketQty = document.getElementById("dlslot_ticket_qty");
+    const email = document.getElementById("dlslot_email");
+    const month = document.getElementById("dlslot_dob_month");
+    const day = document.getElementById("dlslot_dob_day");
+    const year = document.getElementById("dlslot_dob_year");
+    const zip = document.getElementById("dlslot_zip");
+    const country = document.getElementById("dlslot_country");
+    const tos = document.getElementById("dlslot_agree");
 
-  firstName.value = "Peter";
-  lastName.value = "Zhen";
-  ticketQty.options.selectedIndex = 2;
-  email.value = "peterzhen0713@gmail.com";
-  month.value = "07";
-  day.value = "13";
-  year.value = "1989";
-  zip.value = "11370";
-  country.options.selectedIndex = profile.country.selectedIndex;
-  tos.checked = true;
+    firstName.value = storage.profile.fname;
+    lastName.value = storage.profile.lname;
+    ticketQty.options.selectedIndex = storage.profile.ticketQty;
+    email.value = storage.profile.email;
+    month.value = storage.profile.month;
+    day.value = storage.profile.day;
+    year.value = storage.profile.year;
+    zip.value = storage.profile.zip;
+    country.options.selectedIndex = storage.profile.selectedIndex;
+    tos.checked = true;
+  }
 };
 
-chrome.storage.sync.get("profile", profile => {
-  fillForm(profile);
-  console.log(profile);
+chrome.storage.sync.get("profile", storage => {
+  fillForm(storage);
 });
--- popup.js
+++ popup.js
@@ -1,17 +1,31 @@
+const firstName = document.getElementById("fNameInput");
+const lastName = document.getElementById("lNameInput");
+const ticketQty = document.getElementById("ticketQuantityInput");
+const email = document.getElementById("emailInput");
+const month = document.getElementById("monthInput");
+const day = document.getElementById("dayInput");
+const year = document.getElementById("yearInput");
+const zip = document.getElementById("zipInput");
+const country = document.getElementById("countryInput");
+
 const backButton = document.getElementById("back-button");
 const saveButton = document.getElementById("save-button");
 
-const saveProfile = () => {
-  const firstName = document.getElementById("fNameInput");
-  const lastName = document.getElementById("lNameInput");
-  const ticketQty = document.getElementById("ticketQuantityInput");
-  const email = document.getElementById("emailInput");
-  const month = document.getElementById("monthInput");
-  const day = document.getElementById("dayInput");
-  const year = document.getElementById("yearInput");
-  const zip = document.getElementById("zipInput");
-  const country = document.getElementById("countryInput");
+chrome.storage.sync.get("profile", storage => {
+  if (storage.profile){
+    firstName.value = storage.profile.fname;
+    lastName.value = storage.profile.lname;
+    ticketQty.selectedIndex = storage.profile.ticketQty;
+    email.value = storage.profile.email;
+    month.value = storage.profile.month;
+    day.value = storage.profile.day;
+    year.value = storage.profile.year;
+    zip.value = storage.profile.zip;
+    country.selectedIndex = storage.profile.selectedIndex;
+  }
+});
 
+const saveProfile = () => {
   const profile = {
     "fname": firstName.value,
     "lname": lastName.value,
